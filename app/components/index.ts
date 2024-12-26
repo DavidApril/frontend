bdade8eb9a66768fdda3ab846e6f73fd7888a61d
David <davidagamez04@gmail.com>
2024-12-26 16:09:29 -0500

    feat: add comment functionality by exporting comment-related components and interfaces

--- app/components/index.ts
+++ app/components/index.ts
@@ -4,6 +4,8 @@ export * from './projects/create-project-dialog';
 export * from './tasks/table-tasks';
 export * from './tasks/create-task-dialog';
 export * from './tasks/edit-task-dialog';
+export * from './tasks/comment';
+export * from './tasks/create-comment';
 
 export * from './teams/create-team-dialog';
 
--- core/interfaces/index.ts
+++ core/interfaces/index.ts
@@ -4,3 +4,4 @@ export * from './auth.interface';
 export * from './http-response.interface';
 export * from './teams.interface';
 export * from './users.interfaces';
+export * from './comments.interface';
