bd440d0c923f2a31c96dedfb391a824a3c64b6d2
David <davidagamez04@gmail.com>
2024-12-26 16:07:33 -0500

    feat: integrate Auth component into Home page for user authentication

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { ProjectsService } from '@/core/services';
 import { CreateProjectDialog, CreateTeamDialog, Profile, TableProject } from './components';
+import { Auth } from './components/auth';
 
 export default async function Home() {
 	const projectsData = await ProjectsService.getProjects();
@@ -8,6 +9,7 @@ export default async function Home() {
 	return (
 		<div className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
 			<main className='flex flex-col gap-8 row-start-2 items-center sm:items-start'>
+				<Auth />
 				<h1 className='text-2xl font-bold'>Welcome to the project management system</h1>
 				<nav className='flex gap-2'>
 					<CreateProjectDialog />
