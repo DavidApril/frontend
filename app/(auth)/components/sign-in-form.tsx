cadd9081bd6f821c376a18ec0c2ecb3a533df37a
David <davidagamez04@gmail.com>
2025-01-12 16:31:27 -0500

    feat: refactor sign-in form and integrate authentication context

--- app/(auth)/components/sign-in-form.tsx
+++ app/(auth)/components/sign-in-form.tsx
@@ -1,83 +1,69 @@
-'use client';
-import { AuthService } from '@/core/services';
-import { Button, Input } from '@/core/components';
-import { useAuthStore } from '@/core/store';
-import { useFormik } from 'formik';
-import { useRouter } from 'next/navigation';
-import { useState } from 'react';
+"use client";
 
-import * as Yup from 'yup';
-import clsx from 'clsx';
+import { Button, Input } from "@/core/components";
+import { ErrorMessage, Form, Formik } from "formik";
+import { useContext } from "react";
+import { authContext } from "@/core/context";
+import clsx from "clsx";
+
+import * as Yup from "yup";
+import { FErrorMessage } from "@/core/components/ui/formik-error-message";
 
 export const SignInForm = () => {
-	const router = useRouter();
+  const { sign_in, loading } = useContext(authContext);
 
-	const { setUser, setToken } = useAuthStore();
+  const initialValues = {
+    email: "admin@example.com",
+    password: "Abc123456@",
+  };
 
-	const [loading, setLoading] = useState(false);
-	const [errors, setErrors] = useState<boolean>(false);
+  const validationSchema = Yup.object().shape({
+    email: Yup.string()
+      .email("Invalid email address")
+      .required("Email is required"),
+    password: Yup.string().required("Password is required"),
+  });
 
-	const initialValues = {
-		email: 'admin@example.com',
-		password: 'Abc123456@',
-	};
+  const onSubmit = async (credentials: typeof initialValues) =>
+    sign_in(credentials);
 
-	const validationSchema = Yup.object().shape({
-		email: Yup.string().email('Invalid email address').required('Email is required'),
-		password: Yup.string().required('Password is required'),
-	});
-
-	const formik = useFormik({
-		initialValues,
-		validationSchema,
-		onSubmit: async (credentials) => {
-			setLoading(true);
-			await AuthService.signin(credentials)
-				.then(async (r) => {
-					const user = await AuthService.getUser(r.user.id);
-					setUser(user);
-					setToken(r.token);
-					router.replace('/');
-				})
-				.catch(() => {
-					setErrors(true);
-					setTimeout(() => setErrors(false), 3000);
-				})
-				.finally(() => {
-					setLoading(false);
-				});
-		},
-	});
-
-	return (
-		<form
-			className='flex flex-col gap-4'
-			onSubmit={formik.handleSubmit}>
-			<Input
-				invalid={errors}
-				name='email'
-				type='email'
-				placeholder='Email'
-				value={formik.values.email}
-				onChange={formik.handleChange}
-				onBlur={formik.handleBlur}
-			/>
-			<Input
-				invalid={errors}
-				name='password'
-				type='password'
-				placeholder='Password'
-				value={formik.values.password}
-				onChange={formik.handleChange}
-				onBlur={formik.handleBlur}
-			/>
-			<Button
-				type='submit'
-				disabled={errors}
-				loading={loading}
-				className={clsx(errors && 'bg-red-500')}>
-				Sign in
-			</Button>
-		</form>
-	);
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={onSubmit}
+    >
+      {({ values, handleChange, handleBlur, errors }) => (
+        <Form className="flex flex-col gap-4">
+          <Input
+            name="email"
+            type="email"
+            placeholder="Email"
+            value={values.email}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            invalid={!!errors.email}
+          />
+          <FErrorMessage name="email" />
+          <Input
+            name="password"
+            type="password"
+            placeholder="Password"
+            value={values.password}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            invalid={!!errors.password}
+          />
+          <ErrorMessage name="password" />
+          <Button
+            type="submit"
+            loading={loading}
+            className={clsx(errors && "bg-red-500")}
+          >
+            Sign in
+          </Button>
+        </Form>
+      )}
+    </Formik>
+  );
 };
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next';
 import localFont from 'next/font/local';
+
 import './globals.css';
+import { AuthProvider } from '@/core/context';
 
 const geistSans = localFont({
 	src: './fonts/GeistVF.woff',
@@ -24,8 +26,10 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang='en'>
-			<body className={`${geistSans.variable} ${geistMono.variable} antialiased dark`}>{children}</body>
-		</html>
+		<AuthProvider>
+			<html lang='en'>
+				<body className={`${geistSans.variable} ${geistMono.variable} antialiased dark`}>{children}</body>
+			</html>
+		</AuthProvider>
 	);
 }
